738780c008e8e1962639f182fde1d3c0260027b8
krishnaaSundaram-QaStack-1989 <85032384+krishnaaSundaram-QaStack-1989@users.noreply.github.com>
2022-08-25 16:48:53 +0530

    Create testData.ts

---
+++ src/tests/testDatas/testData.ts
@@ -0,0 +1,24 @@
+export const testInputs = {
+ hompageUrl: "https://playwright.dev/docs/test-pom",
+  uname: `krishnaasundaram1989@gmail.com`,
+  pwd: `playwrightpoc@022`,
+  waitForElement: 120000,
+  shortWaits: 5000,
+  mediumWaits: 10000,
+  longWaits: 20000,
+  extendedLongWaits: 50000,
+   
+   //---------------application url
+   
+  url_xx: "hxxxxxxin",
+  uname_xxx: "m.xxxxxxxx@gmail.com",
+  pwd_xxx: "Talent2022!",
+  xxUrl: /.*itsdummysite.com/,
+  dashBoardUrl: /.*dashboard/,
+  timesheetUrl: /.*timesheets/,
+  timesheetPage: {
+    timeSheetTitle: "My Timesheets",
+  },
+  uploadFilePath: "src/attachments/Test evidence.png",
+};
+
