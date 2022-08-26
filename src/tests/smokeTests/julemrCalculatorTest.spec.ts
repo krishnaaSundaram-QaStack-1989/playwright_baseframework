7a65502a71783be7dec87acec05750b09b4cea22
krishnaaSundaram-QaStack-1989 <85032384+krishnaaSundaram-QaStack-1989@users.noreply.github.com>
2022-08-26 13:39:50 +0530

    Create julemrCalculatorTest.spec.ts

---
+++ src/tests/smokeTests/julemrCalculatorTest.spec.ts
@@ -0,0 +1,61 @@
+import { test, Page } from "@playwright/test";
+import { pom } from "@pages/pom";
+import { WebActions } from "@lib/WebActions";
+import { juliemr } from "@pages/julieCalculatorPage";
+import { testInputs } from "@testDatas/testData";
+
+test.describe.configure({ mode: "serial" });
+let page: Page;
+
+test.beforeAll(async ({ browser }) => {
+  page = await browser.newPage();
+});
+
+test.afterAll(async () => {
+  await page.close();
+});
+
+test("Test case 1 - open juliemr calculator url in browser @githubaction ", async () => {
+  const pomPageActions = new pom(page);
+  await pomPageActions.navigateToURL(process.env.juliemrCalc_URL);
+});
+
+test("Test case 2 - verify url of page  @githubaction ", async () => {
+  const actions = new WebActions(page);
+  await actions.checkCurrentPageUrlOrTitle(
+    testInputs.julemrCalcUrl,
+    "url",
+    "url check for the page juliemr calculator"
+  );
+});
+
+test("Test case 3 - enter and submit no for addition  @githubaction ", async () => {
+  const juliemrCalc = new juliemr(page);
+  await juliemrCalc.enterTwoNumbers(testInputs.operations[0]);
+  await juliemrCalc.checkResultRowsAreDisplayed(1);
+});
+
+test("Test case 4 - enter and submit no for subtraction  @githubaction ", async () => {
+  const juliemrCalc = new juliemr(page);
+  await juliemrCalc.enterTwoNumbers(testInputs.operations[1]);
+  await juliemrCalc.checkResultRowsAreDisplayed(2);
+});
+
+test("Test case 5 - enter and submit no for multiplication  @githubaction ", async () => {
+  const juliemrCalc = new juliemr(page);
+  await juliemrCalc.enterTwoNumbers(testInputs.operations[2]);
+  await juliemrCalc.checkResultRowsAreDisplayed(3);
+});
+
+test("Test case 6 - enter and submit no for division  @githubaction ", async () => {
+  const juliemrCalc = new juliemr(page);
+  await juliemrCalc.enterTwoNumbers(testInputs.operations[3]);
+  await juliemrCalc.checkResultRowsAreDisplayed(4);
+});
+
+test("Test case 7 - enter and submit no for modulo  @githubaction ", async () => {
+  const juliemrCalc = new juliemr(page);
+  await juliemrCalc.enterTwoNumbers(testInputs.operations[4]);
+  await juliemrCalc.checkResultRowsAreDisplayed(5);
+  await page.pause();
+});
